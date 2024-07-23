ed56a39994086912017d94b0159bab9376a1ad0d
Levy Lisboa <lisboalevy@gmail.com>
2024-07-23 19:24:29 -0300

    Adicionada Serialização em YML

--- CRUD/src/main/java/com/example/api_gateway/config/WebConfig.java
+++ CRUD/src/main/java/com/example/api_gateway/config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.example.api_gateway.config;
 
+import com.example.api_gateway.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,11 +20,20 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("xml", MediaType.APPLICATION_XML);
     }*/
     // formato via header localhost:8080/api/person/v1
+
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.favorParameter(false).ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
     }
 }
--- CRUD/src/main/java/com/example/api_gateway/controller/PersonController.java
+++ CRUD/src/main/java/com/example/api_gateway/controller/PersonController.java
@@ -3,8 +3,8 @@ package com.example.api_gateway.controller;
 import com.example.api_gateway.data.vo.v1.PersonVO;
 import com.example.api_gateway.exceptions.ResourceNotFoundException;
 import com.example.api_gateway.services.PersonServices;
+import com.example.api_gateway.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +16,26 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable(value = "id") long id) throws ResourceNotFoundException {
         return services.findById(id);
     }
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
     public List<PersonVO> findAll() {
         return services.findAll();
     }
 
     @PostMapping(
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
     public PersonVO create(@RequestBody PersonVO PersonVO) {
         return services.create(PersonVO);
     }
 
     @PutMapping(
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO PersonVO) throws ResourceNotFoundException {
         return services.update(PersonVO);
     }
