ebe09d5dd2a0c824bf18966e1b3a65cfe59e21ea
LevySLisboa <lisboalevy@gmail.com>
2024-09-06 10:27:46 -0300

    Definindo as Exceptions e Response

--- CRUD/src/main/java/com/example/api_gateway/exceptions/MyFileNotFoundException.java
+++ CRUD/src/main/java/com/example/api_gateway/exceptions/MyFileNotFoundException.java
@@ -4,8 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
+public class MyFileNotFoundException extends RuntimeException{
+    public MyFileNotFoundException(String message) {
         super(message);
+    }
+    public MyFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
--- CRUD/src/main/java/com/example/api_gateway/exceptions/RequiredObjectIsNullException.java
+++ CRUD/src/main/java/com/example/api_gateway/exceptions/RequiredObjectIsNullException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class RequiredObjectIsNullException extends Exception{
+public class RequiredObjectIsNullException extends RuntimeException{
     public RequiredObjectIsNullException(String message) {
         super(message);
     }
