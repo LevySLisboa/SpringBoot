5c2bc7ea2fdd14392f374c34deaee47f4121a00d
LevySLisboa <lisboalevy@gmail.com>
2024-09-06 11:47:31 -0300

    Preparando o Serviço para Prover Downloads

--- CRUD/src/main/java/com/example/api_gateway/services/FileStorageService.java
+++ CRUD/src/main/java/com/example/api_gateway/services/FileStorageService.java
@@ -2,7 +2,10 @@ package com.example.api_gateway.services;
 
 import com.example.api_gateway.config.FileStorageConfig;
 import com.example.api_gateway.exceptions.FileStorageException;
+import com.example.api_gateway.exceptions.MyFileNotFoundException;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,22 +25,33 @@ public class FileStorageService {
         this.fileStorageLocation = path;
         try {
             Files.createDirectories(this.fileStorageLocation);
-        }catch (Exception e){
-            throw new FileStorageException("Could not create the directory where the uploaded files will be storage!",e);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where the uploaded files will be storage!", e);
         }
     }
 
-    public String storeFIle(MultipartFile file){
+    public String storeFIle(MultipartFile file) {
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
         try {
-            if(fileName.contains("..")){
+            if (fileName.contains("..")) {
                 throw new FileStorageException(STR."Sorry! File name contains invalid path sequence \{fileName}");
             }
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
-            Files.copy(file.getInputStream(),targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             return fileName;
-        }catch (Exception e){
-            throw new FileStorageException(STR."Could not store file \{fileName}. Please try again!",e);
+        } catch (Exception e) {
+            throw new FileStorageException(STR."Could not store file \{fileName}. Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if(resource.exists()) return resource;
+            else throw new MyFileNotFoundException("File not found!");
+        } catch (Exception e) {
+            throw new MyFileNotFoundException(STR."File not found! \{fileName}", e);
         }
     }
 }
